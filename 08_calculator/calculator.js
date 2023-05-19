feed8f3237188301da285aeb59747a2ff3e1f46c
rincaveski <jrincaveski@gmail.com>
2023-05-19 14:28:15 -0300

    calculadora pronta

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acumulador, atual) => acumulador +=atual, 0)
+}
+
+const multiply = function(arr) {
+  return arr.reduce((acumulador, atual) => acumulador *= atual, 1)
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return a**b
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(valor) {
+	if (valor == 0) return 1
+  return valor * factorial(valor - 1)
 };
 
 // Do not edit below this line
